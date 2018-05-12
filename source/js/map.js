045ffd0f17901f701f9162cd1c496a026c95a0ab
Moreorsea <dastast@mail.ru>
2018-05-12 15:53:31 +0300

    Добавила пути к картинкам

--- source/js/map.js
+++ source/js/map.js
@@ -5,12 +5,12 @@ ymaps.ready(init);
   function init(){
     myMap = new ymaps.Map("map", {
       center: [59.93863106, 30.32305450],
-      zoom: 20
+      zoom: 7
   });
 
   myPlacemark = new ymaps.Placemark([59.93863106, 30.32305450], {
-    hintContent: 'Москва!',
-    balloonContent: 'Столица России'
+    hintContent: 'Питер',
+    balloonContent: 'Вторая столица России'
   });
 
   myMap.geoObjects.add(myPlacemark);
