3021bd6e31b5007fe1da54470fd18c027a9754a4
Moreorsea <dastast@mail.ru>
2018-05-12 15:10:34 +0300

    Делала, чтобы походило на макет

--- source/js/map.js
+++ source/js/map.js
@@ -1,9 +1,17 @@
-    ymaps.ready(init);
-    var myMap;
+ymaps.ready(init);
+  var myMap,
+    myPlacemark;
 
-    function init(){
-        myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 7
-        });
-    }
+  function init(){
+    myMap = new ymaps.Map("map", {
+      center: [59.93863106, 30.32305450],
+      zoom: 20
+  });
+
+  myPlacemark = new ymaps.Placemark([59.93863106, 30.32305450], {
+    hintContent: 'Москва!',
+    balloonContent: 'Столица России'
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
