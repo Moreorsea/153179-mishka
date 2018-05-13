1f0097ab3e5af3ece8e32c3347828798e2a9f53f
Moreorsea <dastast@mail.ru>
2018-05-13 09:18:52 +0300

    Сделала картинку фоном в блоке промо

--- source/js/map.js
+++ source/js/map.js
@@ -5,7 +5,7 @@ ymaps.ready(init);
   function init(){
     myMap = new ymaps.Map("map", {
       center: [59.93863106, 30.32305450],
-      zoom: 7
+      zoom: 10
   });
 
   myPlacemark = new ymaps.Placemark([59.93863106, 30.32305450], {
